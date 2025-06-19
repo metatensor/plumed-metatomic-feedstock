53844045006f9782a0603fb3084bd0d354526b04
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-06-19 11:37:45 +0200

    Enable metatomic integration

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,17 +19,28 @@ fi
 # we also store path so that software linking libplumedWrapper.a knows where libplumedKernel can be found.
 export CPPFLAGS="-D__PLUMED_DEFAULT_KERNEL=$PREFIX/lib/libplumedKernel$SHLIB_EXT $CPPFLAGS"
 
+# libtorch puts some headers in a non-standard place
+export CPPFLAGS="-I$PREFIX/include/torch/csrc/api/include $CPPFLAGS"
+
 # enable optimization
 export CXXFLAGS="${CXXFLAGS//-O2/-O3}"
 
 # libraries are explicitly listed here due to --disable-libsearch
 export LIBS="-lfftw3 -lgsl -lgslcblas -llapack -lblas -lz $LIBS"
+export LIBS="-lmetatomic_torch -lmetatensor_torch -lmetatensor -ltorch -lc10 -ltorch_cpu  $LIBS"
 
 # python is disabled since it should be provided as a separate package
 # --disable-libsearch forces to link only explicitely requested libraries
 # --disable-static-patch avoid tests that are only required for static patches
 # --disable-static-archive makes package smaller
-./configure --prefix=$PREFIX --disable-python --disable-libsearch --disable-static-patch --disable-static-archive
+./configure --prefix=$PREFIX \
+            --disable-python \
+            --disable-libsearch \
+            --disable-static-patch \
+            --disable-static-archive \
+            --enable-modules=all \
+            --enable-libmetatomic \
+            --enable-libtorch
 
 make -j${CPU_COUNT}
 make install
